f305e98779d8ab4bb4b5965b48833255670efc9e
«SergeyVepr» <lolssan@icloud.com>
2025-11-12 18:47:21 +0200

    Refactor student repository and controller to simplify logic, streamline error handling, and improve code readability across student management functions.

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -1,13 +1,11 @@
-import * as repo from "../repository/studentrepository.js"
-import {DefaultNotFount} from "../constants/defaultNotFount.js";
-
+import * as repo from "../repository/studentRepository.js";
 
 export const addStudent = (req, res) => {
     const success = repo.addStudent(req.body);
     if (success) {
         res.status(204).send();
     } else {
-        res.status(409).send({res: "Student already exists"});
+        res.status(409).send();
     }
 }
 
@@ -22,97 +20,49 @@ export const findStudent = (req, res) => {
 }
 
 export const updateStudent = (req, res) => {
-    try {
-        const student = repo.updateStudent(+req.params.id, req.body.name, req.body.password);
+    const student = repo.updateStudent(+req.params.id, req.body);
+    if (student) {
         const {scores, ...studentWithoutScores} = student;
-        res.status(200).send(studentWithoutScores);
-    } catch (e) {
-        res.status(404).send(
-            new DefaultNotFount(
-                404,
-                e.message,
-                `student with id ${req.params.id} not found`,
-                req.path
-            )
-        );
+        res.json(studentWithoutScores);
+    } else {
+        res.status(404).send();
     }
 }
 
 export const deleteStudent = (req, res) => {
-    try {
-        const student = repo.deleteStudent(+req.params.id);
-        const {password, ...studentWithOutPassword} = student;
-        res.status(200).send(studentWithOutPassword);
-    } catch (e) {
-        res.status(404).send(
-            new DefaultNotFount(
-                404,
-                e.message,
-                `student with id ${req.params.id} not found`,
-                req.path
-            )
-        );
+    const student = repo.deleteStudent(+req.params.id);
+    if (student) {
+        const {password, ...studentWithoutPassword} = student;
+        res.json(studentWithoutPassword);
+    } else {
+        res.status(404).send();
     }
 }
 
 export const addScore = (req, res) => {
-    try {
-        repo.addScore(+req.params.id, req.body);
+    const success = repo.addScore(+req.params.id, req.body.examName, +req.body.score);
+    if (success) {
         res.status(204).send();
-    } catch (e) {
-        res.status(404).send(
-            new DefaultNotFount(
-                404,
-                e.message,
-                `student not found`,
-                req.path
-            )
-        );
+    } else {
+        res.status(404).send();
     }
 }
 
 export const findByName = (req, res) => {
-    try {
-        const students = repo.findByName(req.params.name);
-        res.json(students.map(student => {
-            const {password, ...studentWithoutPassword} = student;
-            return studentWithoutPassword;
-        }));
-    } catch (e) {
-        res.status(404).send(
-            new DefaultNotFount(
-                404,
-                e.message,
-                `student not found`,
-                req.path
-            )
-        );
-    }
-
+    const students = repo.findByName(req.params.name);
+    const studentsWithoutPasswords = students.map(student => ({...student, password: undefined}))
+    res.json(studentsWithoutPasswords);
 }
 
 export const countByNames = (req, res) => {
-    const names = [].concat(req.query.names);
-    const count = repo.countByNames(names);
-    res.json(count);
+    const names = req.query.names;
+    const list = Array.isArray(names) ? names : [names];
+    const count = repo.countByNames(list);
+    res.json(count)
 }
 
 export const findByMinScore = (req, res) => {
-    try {
-        const students = repo.findByMinScore(req.params.exam, +req.params.minScore);
-        res.json(students.map(student => {
-            const {password, ...studentWithoutPassword} = student;
-            return studentWithoutPassword;
-        }));
-    } catch (e) {
-        res.status(404).send(
-            new DefaultNotFount(
-                404,
-                e.message,
-                `student not found`,
-                req.path
-            )
-        );
-    }
-
+    const students = repo.findByMinScore(req.params.exam, +req.params.minScore);
+    const studentsWithoutPasswords = students.map(student => ({...student, password: undefined}))
+    res.json(studentsWithoutPasswords);
 }
--- src/repository/studentrepository.js
+++ src/repository/studentrepository.js
@@ -1,62 +1,52 @@
-import {Student} from "../model/student.js";
+import { Student } from "../model/student.js";
 
 const students = new Map();
 
 export const addStudent = ({id, name, password}) => {
-    if (students.has(id)) return false;
+    if(students.has(id)) {
+        return false;
+    }
     students.set(id, new Student(id, name, password));
     return true;
-};
-
-export const findStudent = (id) => students.get(+id);
-
-export const updateStudent = (id, name, password, scores) => {
-    const student = students.get(id);
-    if (!student) throw new Error("Student not found");
-    const updateStudent = {...student, ...{name, password, scores}};
-    students.set(id, updateStudent);
-    return students.get(id);
 }
 
-export const deleteStudent = (id) => {
+export const findStudent = id => students.get(id);
+
+export const deleteStudent = id => {
     const student = students.get(id);
     if (student) {
         students.delete(id);
         return student;
-    } else {
-        throw new Error("Student not found");
     }
-
 }
 
-export const addScore = (id, {examName, score}) => {
+export const updateStudent = (id, data) => {
     const student = students.get(id);
-    if (!student) throw new Error("Not found");
-    student.scores[examName] = score;
-    return student;
+    if (student) {
+        Object.assign(student, data);
+        return student;
+    }
+}
+
+export const addScore = (id, exam, score) => {
+    const student = students.get(id);
+    if (student) {
+        student.scores[exam] = score;
+        return true;
+
+    }
+    return false;
 }
 
 export const findByName = (name) => {
-    const arr = Array.from(students.values()).filter(student => student.name.toLowerCase() === name.toLowerCase());
-    if (arr.length === 0) throw new Error("Not found");
-    return arr;
+    return Array.from(students.values()).filter(s => s.name.toLowerCase() === name.toLowerCase());
 }
 
 export const countByNames = (names) => {
-    const namesLowerCase = names.map(name => name.toLowerCase());
-    const arr = Array.from(students.values()).filter(
-        student => namesLowerCase.includes(student.name.toLowerCase())
-    );
-    return arr.length;
+    names = names.map(name => name.toLowerCase());
+    return Array.from(students.values()).filter(s => names.includes(s.name.toLowerCase())).length;
 }
 
-export const findByMinScore = (examName, minScore) => {
-    const arr = Array.from(students.values()).filter(
-        student => student.scores[examName] >= minScore
-    );
-    if(arr.length === 0) throw new Error(
-        `No student with min score ${minScore} in exam ${examName}`
-    )
-    return arr;
+export const findByMinScore = (exam, minScore) => {
+    return Array.from(students.values()).filter(s => s.scores[exam] >= minScore);
 }
-
